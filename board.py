de6fc4a341e309185633534a46736dd46f60edfa
recallryan <ryan@recalltheapp.com>
2022-11-28 01:17:49 +0000

    working minimax

--- board.py
+++ board.py
@@ -9,30 +9,47 @@ class Game:
         self.p2 = []
         self.k = k
         self.occupied_cells = []
+        self.cells = [(x, y) for x in range(1, self.m + 1) for y in range(1, self.n + 1)]
 
     def initialize_game(self):
         return self.m, self.n
 
-    def max(self):
-        x = int(input('P1, Please input row number'))
-        y = int(input('P1, Please input column number'))
-        while not self.is_valid((x,y)):
-            print('Invalid')
+    def get_possible_states(self, available_cells, occupied_cells):
+        possible_states = list(set(available_cells) - set(occupied_cells))
+        possible_states.sort()
+        return possible_states
+
+    def max(self, auto=False):
+        bestMove = self.find_best_max_move()
+        print("The Optimal Move is :", bestMove)
+        if auto:
+            self.p1.append(bestMove)
+            self.occupied_cells.append(bestMove)
+        else:
             x = int(input('P1, Please input row number'))
             y = int(input('P1, Please input column number'))
-        self.p1.append((x,y))
-        self.occupied_cells.append((x, y))
+            while not self.is_valid((x, y)):
+                print('Invalid')
+                x = int(input('P1, Please input row number'))
+                y = int(input('P1, Please input column number'))
+            self.p1.append((x, y))
+            self.occupied_cells.append((x, y))
 
-    def min(self):
-        x = int(input('P2, Please input row number'))
-        y = int(input('P2, Please input column number'))
-        while not self.is_valid((x,y)):
-            print('Invalid')
+    def min(self, auto=False):
+        bestMove = self.find_best_min_move()
+        print("The Optimal Move is :", bestMove)
+        if auto:
+            self.p2.append(bestMove)
+            self.occupied_cells.append(bestMove)
+        else:
             x = int(input('P2, Please input row number'))
             y = int(input('P2, Please input column number'))
-        self.p2.append((x,y))
-        self.occupied_cells.append((x, y))
-
+            while not self.is_valid((x, y)):
+                print('Invalid')
+                x = int(input('P2, Please input row number'))
+                y = int(input('P2, Please input column number'))
+            self.p2.append((x, y))
+            self.occupied_cells.append((x, y))
 
     def drawboard(self):
         x_dim, y_dim = self.initialize_game()
@@ -120,20 +137,83 @@ class Game:
 
         for each_list in total:
             if len(set(self.p1).intersection(set(each_list))) == self.k:
-                return True
+                return 10
             if len(set(self.p2).intersection(set(each_list))) == self.k:
-                return True
-        return False
+                return -10
+
+        if len(self.cells) == len(self.occupied_cells):
+            return False
+
+    def find_best_max_move(self):
+        bestScore = float('-inf')
+        bestMove = None
+        for state in self.get_possible_states(self.cells, self.occupied_cells):
+            self.p1.append(state)
+            self.occupied_cells.append(state)
+            score = self.minimax(0, False)
+            self.p1.remove(state)
+            self.occupied_cells.remove(state)
+            if (score > bestScore):
+                bestScore = score
+                bestMove = state
+        return bestMove
+
+    def find_best_min_move(self):
+        bestScore = float('inf')
+        bestMove = None
+        for state in self.get_possible_states(self.cells, self.occupied_cells):
+            self.p2.append(state)
+            self.occupied_cells.append(state)
+            score = self.minimax(0, True)
+            self.p2.remove(state)
+            self.occupied_cells.remove(state)
+            if (score < bestScore):
+                bestScore = score
+                bestMove = state
+        return bestMove
+
+    def minimax(self, depth, is_max):
+        score = self.is_terminal()
+
+        if score == 10:
+            return score
+
+        if score == -10:
+            return score
+
+        if len(self.get_possible_states(self.cells, self.occupied_cells)) == 0:
+            return 0
+
+        if is_max:
+            value = float('-inf')
+            for state in self.get_possible_states(self.cells, self.occupied_cells):
+                self.p1.append(state)
+                self.occupied_cells.append(state)
+                value = max(value, self.minimax(depth + 1, not is_max))
+                self.p1.remove(state)
+                self.occupied_cells.remove(state)
+            return value
+
+        else:
+            value = float('inf')
+            for state in self.get_possible_states(self.cells, self.occupied_cells):
+                self.p2.append(state)
+                self.occupied_cells.append(state)
+                value = min(value, self.minimax(depth + 1, not is_max))
+                self.p2.remove(state)
+                self.occupied_cells.remove(state)
+            return value
+
 
 if __name__ == '__main__':
 
     board = Game(3, 3, 3)
 
     while not board.is_terminal():
-        board.max()
+        board.max(auto=True)
         board.drawboard()
         if board.is_terminal():
             break
-        board.min()
+        board.min(auto=True)
         board.drawboard()
 
